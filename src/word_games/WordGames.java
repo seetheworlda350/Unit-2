591818f22e15b7521da14880e2f90fc38ce61698
smithbwlhs <smithb@wlwv.k12.or.us>
2024-10-10 10:07:49 -0700

    updated javadocs for WordGames

--- src/word_games/WordGames.java
+++ src/word_games/WordGames.java
@@ -1,5 +1,11 @@
 package word_games;
 
+/**
+ * Name: Brandon Smith (put your name here)
+ * Date: 10-10-24 (put date of last revision here)
+ * Description: This is a collection of methods for a word game that
+ * people can play
+ */
 public class WordGames
 {
     private String word;
@@ -9,6 +15,10 @@ public class WordGames
         word = text;
     }
 
+    /**
+     * Scrambles a word by switching the first and second half of the word.
+     * @return the scrambled word
+     */
     public String scramble()
     {
         // switch first half
@@ -17,7 +27,12 @@ public class WordGames
 
     }
 
-
+    /**
+     * Modifies a given string by inserting text at the specified index
+     * @param insertIdx int index where the text will be inserted
+     * @param insertText String text to be inserted
+     * @return modified text
+     */
     public String bananaSplit(int insertIdx, String insertText)
     {
         // Insert insertText at the position
@@ -26,7 +41,13 @@ public class WordGames
 
     }
 
-
+    /**
+     * Modifies a given string by inserting text at the first occurrence of the
+     * specified character
+     * @param insertChar char character you want
+     * @param insertText String
+     * @return the modified text
+     */
     public String bananaSplit(String insertChar, String insertText)
     {
         // Insert insertText after the first
@@ -35,7 +56,7 @@ public class WordGames
 
     }
 
-
+    @Override
     public String toString()
     {
         // Games[word]
