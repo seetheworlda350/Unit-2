09a42f60b68271d3f416e7e0cb83233b00a534d1
smithbwlhs <smithb@wlwv.k12.or.us>
2024-10-09 18:40:23 -0700

    initial commit

---
+++ src/math_class/Main.java
@@ -0,0 +1,11 @@
+package math_class;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //using math class
+        //create a random number in the range [0,1)
+
+        //create
+    }
+}
---
+++ src/word_games/WordGames.java
@@ -0,0 +1,46 @@
+package word_games;
+
+public class WordGames
+{
+    private String word;
+
+    public WordGames(String text)
+    {
+        word = text;
+    }
+
+    public String scramble()
+    {
+        // switch first half
+        // and second half
+        return "";
+
+    }
+
+
+    public String bananaSplit(int insertIdx, String insertText)
+    {
+        // Insert insertText at the position
+        // insertIdx
+        return "";
+
+    }
+
+
+    public String bananaSplit(String insertChar, String insertText)
+    {
+        // Insert insertText after the first
+        // occurence of the insertChar
+        return "";
+
+    }
+
+
+    public String toString()
+    {
+        // Games[word]
+        return "";
+
+    }
+
+}
